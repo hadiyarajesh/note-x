9c72b280f0c3967116fdf2328d356b08a5077e11
Hritik Kumar Singh <singhhritik978@gmail.com>
2022-09-03 01:02:06 +0530

    Fixed spacing between two row elems

---
+++ app/src/main/java/com/hadiyarajesh/notex/ui/note/note_folder/NoteFolderItem.kt
@@ -0,0 +1,15 @@
+package com.hadiyarajesh.notex.ui.note.note_folder
+
+import android.graphics.Color
+import com.hadiyarajesh.notex.database.entity.Note
+import java.time.Instant
+
+data class NoteFolderItem(
+    val folderId: Long,
+    val name: String? = null,
+    val description: String? = null,
+    val notes: List<Note>,
+    val color: Color? = null,
+    val createdOn: Instant,
+    val updatedOn: Instant
+)
--- app/src/main/java/com/hadiyarajesh/notex/ui/note/note_folder/NoteFolderScreen.kt
+++ app/src/main/java/com/hadiyarajesh/notex/ui/note/note_folder/NoteFolderScreen.kt
@@ -1,30 +1,36 @@
 package com.hadiyarajesh.notex.ui.note.note_folder
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.aspectRatio
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.grid.rememberLazyGridState
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.hadiyarajesh.notex.R
 
 @Composable
 fun NoteFolderScreen(
     data: List<NoteFolderItem>
 ) {
+//    val data = listOf(
+//        NoteFolderItem("Android", 15),
+//        NoteFolderItem("Web", 1),
+//        NoteFolderItem("Cloud", 21),
+//        NoteFolderItem("AWS", 12),
+//        NoteFolderItem("Node", 5),
+//        NoteFolderItem("Cyber", 151),
+//    )
     val state = rememberLazyGridState()
     Surface {
         LazyVerticalGrid(
@@ -37,10 +43,10 @@ fun NoteFolderScreen(
             items(
                 items = data,
                 key = {
-                    it.title
+                    it.folderId
                 }
             ) { noteItem ->
-                NoteFolderItemUI(onClick = {}, data = noteItem.title)
+                NoteFolderItemUI(onClick = {}, data = noteItem)
             }
         }
     }
@@ -50,7 +56,7 @@ fun NoteFolderScreen(
 fun NoteFolderItemUI(
     count: Int = 100,
     onClick: () -> Unit,
-    data: String
+    data: NoteFolderItem
 ) {
     Box(
         modifier = Modifier
@@ -60,41 +66,78 @@ fun NoteFolderItemUI(
             .background(Color.LightGray),
         contentAlignment = Alignment.Center
     ) {
-        Text(
-            textAlign = TextAlign.Center,
-            text = "Item $data"
-        )
+        Column(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = "Item ${data.name}",
+                style = MaterialTheme.typography.bodyLarge,
+                fontWeight = FontWeight.Bold
+            )
+            Text(
+                text = "${data.notes.size} notes",
+                style = MaterialTheme.typography.bodyMedium
+            )
+        }
     }
 }
-
-data class NoteFolderItem(
-    val title: String
-)
 
 @Preview
 @Composable
 private fun NoteFolderScreenPreview() {
     val state = rememberLazyGridState()
     Surface {
-        LazyVerticalGrid(
-            columns = GridCells.Fixed(2),
-            verticalArrangement = Arrangement.spacedBy(8.dp),
-            horizontalArrangement = Arrangement.spacedBy(8.dp),
-            state = state
+        Column(
+            modifier = Modifier.fillMaxSize()
         ) {
-            items(100) { noteFolderItems ->
-                Box(
-                    modifier = Modifier
-                        .padding(8.dp)
-                        .aspectRatio(1f)
-                        .clip(RoundedCornerShape(8.dp))
-                        .background(Color.LightGray),
-                    contentAlignment = Alignment.Center
-                ) {
-                    Text(
-                        textAlign = TextAlign.Center,
-                        text = "Item $noteFolderItems"
-                    )
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(
+                    text = "Categories",
+                    style = MaterialTheme.typography.headlineMedium,
+                    modifier = Modifier.padding(start = 120.dp)
+                )
+                Icon(
+                    painter = painterResource(id = R.drawable.ic_add_circle_outline),
+                    contentDescription = "add_note",
+                    modifier = Modifier.padding(end = 10.dp)
+                )
+
+            }
+            LazyVerticalGrid(
+                columns = GridCells.Fixed(2),
+                verticalArrangement = Arrangement.spacedBy(8.dp),
+                horizontalArrangement = Arrangement.spacedBy(8.dp),
+                state = state
+            ) {
+                items(100) { noteFolderItems ->
+                    Box(
+                        modifier = Modifier
+                            .padding(8.dp)
+                            .aspectRatio(1f)
+                            .clip(RoundedCornerShape(8.dp))
+                            .background(Color.LightGray),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        Column(
+                            modifier = Modifier.fillMaxWidth(),
+                            horizontalAlignment = Alignment.CenterHorizontally
+                        ) {
+                            Text(
+                                text = "Item $noteFolderItems",
+                                style = MaterialTheme.typography.bodyLarge,
+                                fontWeight = FontWeight.Bold
+                            )
+                            Text(
+                                text = "$noteFolderItems notes",
+                                style = MaterialTheme.typography.bodyMedium
+                            )
+                        }
+                    }
                 }
             }
         }
