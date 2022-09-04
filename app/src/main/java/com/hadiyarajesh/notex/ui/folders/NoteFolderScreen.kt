492028411feb22c243d479b2d98fb496a7260b35
Hritik Kumar Singh <singhhritik978@gmail.com>
2022-09-04 16:37:54 +0530

    Added routes and deleted redundant data classes

--- app/src/main/java/com/hadiyarajesh/notex/ui/folders/NoteFolderScreen.kt
+++ app/src/main/java/com/hadiyarajesh/notex/ui/folders/NoteFolderScreen.kt
@@ -1,52 +1,176 @@
 package com.hadiyarajesh.notex.ui.folders
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.grid.rememberLazyGridState
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.*
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import androidx.paging.compose.collectAsLazyPagingItems
 import com.hadiyarajesh.notex.R
+import com.hadiyarajesh.notex.database.entity.Folder
+import com.hadiyarajesh.notex.database.model.FolderType.NoteFolder
+import com.hadiyarajesh.notex.ui.navigation.Screens
+import java.time.Instant
 
 @Composable
 fun NoteFolderScreen(
-    data: List<NoteFolderItem>
+    modifier: Modifier = Modifier,
+    navController: NavController,
+    folderViewModel: NoteFolderViewModel
 ) {
-//    val data = listOf(
-//        NoteFolderItem("Android", 15),
-//        NoteFolderItem("Web", 1),
-//        NoteFolderItem("Cloud", 21),
-//        NoteFolderItem("AWS", 12),
-//        NoteFolderItem("Node", 5),
-//        NoteFolderItem("Cyber", 151),
-//    )
+    val scope = rememberCoroutineScope()
+    val context = LocalContext.current
+    val folders = remember { folderViewModel.getFolders(NoteFolder) }.collectAsLazyPagingItems()
+
+    val data = listOf(
+        Folder(
+            folderId = 12345,
+            title = "Android",
+            description = "Android Notes",
+            folderType = NoteFolder,
+            createdOn = Instant.now(),
+            updatedOn = Instant.now()
+        ),
+        Folder(
+            folderId = 12346,
+            title = "Android1",
+            description = "Android Notes",
+            folderType = NoteFolder,
+            createdOn = Instant.now(),
+            updatedOn = Instant.now()
+        ),
+        Folder(
+            folderId = 12347,
+            title = "Android2",
+            description = "Android Notes",
+            folderType = NoteFolder,
+            createdOn = Instant.now(),
+            updatedOn = Instant.now()
+        ),
+        Folder(
+            folderId = 12348,
+            title = "Android3",
+            description = "Android Notes",
+            folderType = NoteFolder,
+            createdOn = Instant.now(),
+            updatedOn = Instant.now()
+        ),
+        Folder(
+            folderId = 12349,
+            title = "Android4",
+            description = "Android Notes",
+            folderType = NoteFolder,
+            createdOn = Instant.now(),
+            updatedOn = Instant.now()
+        ),
+        Folder(
+            folderId = 12350,
+            title = "Android5",
+            description = "Android Notes",
+            folderType = NoteFolder,
+            createdOn = Instant.now(),
+            updatedOn = Instant.now()
+        ),
+        Folder(
+            folderId = 12351,
+            title = "Android6",
+            description = "Android Notes",
+            folderType = NoteFolder,
+            createdOn = Instant.now(),
+            updatedOn = Instant.now()
+        ),
+        Folder(
+            folderId = 12352,
+            title = "Android7",
+            description = "Android Notes",
+            folderType = NoteFolder,
+            createdOn = Instant.now(),
+            updatedOn = Instant.now()
+        ),
+        Folder(
+            folderId = 12352,
+            title = "Android8",
+            description = "Android Notes",
+            folderType = NoteFolder,
+            createdOn = Instant.now(),
+            updatedOn = Instant.now()
+        ),
+    )
     val state = rememberLazyGridState()
     Surface {
-        LazyVerticalGrid(
-            columns = GridCells.Fixed(2),
-            verticalArrangement = Arrangement.spacedBy(8.dp),
-            horizontalArrangement = Arrangement.spacedBy(8.dp),
-            state = state,
-            modifier = Modifier.padding(8.dp)
+        Column(
+            modifier = modifier.fillMaxSize()
         ) {
-            items(
-                items = data,
-                key = {
-                    it.folderId
+            Row(
+                modifier = modifier
+                    .fillMaxWidth()
+                    .padding(10.dp),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically,
+            ) {
+                Text(
+                    text = "Categories",
+                    style = MaterialTheme.typography.headlineMedium,
+                    modifier = modifier.padding(start = 120.dp)
+                )
+                Icon(
+                    painter = painterResource(id = R.drawable.ic_add_circle_outline),
+                    contentDescription = "add_note",
+                    modifier = modifier
+                        .padding(end = 10.dp)
+                        .clickable {
+                            navController.navigate(Screens.AddNote.route)
+                        }
+                )
+
+            }
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(20.dp),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(
+                    text = "List Categories",
+                    style = MaterialTheme.typography.bodyMedium
+                )
+            }
+            LazyVerticalGrid(
+                modifier = modifier.padding(8.dp),
+                columns = GridCells.Fixed(2),
+                verticalArrangement = Arrangement.spacedBy(8.dp),
+                horizontalArrangement = Arrangement.spacedBy(8.dp),
+                state = state,
+            ) {
+                items(
+                    items = data,
+                    key = {
+                        it.title
+                    }
+                ) { noteItem ->
+                    NoteFolderItemUI(onClick = {}, data = noteItem)
                 }
-            ) { noteItem ->
-                NoteFolderItemUI(onClick = {}, data = noteItem)
             }
         }
     }
@@ -54,12 +178,12 @@ fun NoteFolderScreen(
 
 @Composable
 fun NoteFolderItemUI(
-    count: Int = 100,
+    modifier: Modifier = Modifier,
     onClick: () -> Unit,
-    data: NoteFolderItem
+    data: Folder,
 ) {
     Box(
-        modifier = Modifier
+        modifier = modifier
             .padding(8.dp)
             .aspectRatio(1f)
             .clip(RoundedCornerShape(8.dp))
@@ -67,16 +191,16 @@ fun NoteFolderItemUI(
         contentAlignment = Alignment.Center
     ) {
         Column(
-            modifier = Modifier.fillMaxWidth(),
+            modifier = modifier.fillMaxWidth(),
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
             Text(
-                text = "Item ${data.name}",
+                text = "Item ${data.title}",
                 style = MaterialTheme.typography.bodyLarge,
                 fontWeight = FontWeight.Bold
             )
             Text(
-                text = "${data.notes.size} notes",
+                text = "${data.description}",
                 style = MaterialTheme.typography.bodyMedium
             )
         }
@@ -92,21 +216,36 @@ private fun NoteFolderScreenPreview() {
             modifier = Modifier.fillMaxSize()
         ) {
             Row(
-                modifier = Modifier.fillMaxWidth(),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(10.dp),
                 horizontalArrangement = Arrangement.SpaceBetween,
                 verticalAlignment = Alignment.CenterVertically
             ) {
                 Text(
                     text = "Categories",
                     style = MaterialTheme.typography.headlineMedium,
-                    modifier = Modifier.padding(start = 120.dp)
+                    modifier = Modifier.padding(start = 130.dp)
                 )
                 Icon(
                     painter = painterResource(id = R.drawable.ic_add_circle_outline),
                     contentDescription = "add_note",
-                    modifier = Modifier.padding(end = 10.dp)
+                    modifier = Modifier
+                        .padding(end = 10.dp)
                 )
 
+            }
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(10.dp),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(
+                    text = "List Categories",
+                    style = MaterialTheme.typography.bodyMedium
+                )
             }
             LazyVerticalGrid(
                 columns = GridCells.Fixed(2),
