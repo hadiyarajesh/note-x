7d3e5fdb452c71372537f01a5f7c5f143edffa67
Rajesh Hadiya <rajesh.hadiya@outlook.com>
2022-09-10 17:02:11 +0530

    Branch hadiyarajesh (#55)
    * Added new line in Constants.kt
    
    * Note card ui component added which will be inflated in lazy column to show all notes (#35)
    
    * Added CODEOWNERS files (#33)
    
    * Adding CONTRIBUTING.md
    
    * Added CONTRIBUTING.md and squashed commits
    
    * Updated branch naming convention
    
    Updated branch naming convention with github-username
    
    * 1. Added database schema (entity)
    2. Added database functions (dao)
    3. Added how to fetch paging data
    4. Added LazyColumn UI with paging data with different states like loading, success, error
    
    Signed-off-by: Rajesh Hadiya <hadiarajesh007@gmail.com>
    
    * Added CODEOWNERS files. (#19)
    
    * Adding CODEOWNERS files.
    
    Still needed to add the Default reviewer for the two teams.
    
    * Update CODEOWNERS
    
    * fix the validation issues on GitHub.
    
    * updated owners for all the branches.
    
    * updated the Wrong branch combinations.
    
    Signed-off-by: Rajesh Hadiya <hadiarajesh007@gmail.com>
    Co-authored-by: Hritik Kumar Singh <singhhritik978@gmail.com>
    Co-authored-by: Abhinav Suman <abhi.suman@hotmail.com>
    
    * NoteCardView for all notes ui made
    
    * Create main.yml
    
    * Update main.yml
    
    * Update main.yml
    
    * Some improvements done generalised the code
    
    * Some ui improvements and code to inflate all card views added
    
    Signed-off-by: Rajesh Hadiya <hadiarajesh007@gmail.com>
    Co-authored-by: Rajesh Hadiya <hadiarajesh007@gmail.com>
    Co-authored-by: Hritik Kumar Singh <singhhritik978@gmail.com>
    Co-authored-by: Abhinav Suman <abhi.suman@hotmail.com>
    Co-authored-by: Rajesh Hadiya <rajesh.hadiya@outlook.com>
    
    Signed-off-by: Rajesh Hadiya <hadiarajesh007@gmail.com>
    Co-authored-by: Swapnil bhojwani <53923373+swapnil20711@users.noreply.github.com>
    Co-authored-by: Hritik Kumar Singh <singhhritik978@gmail.com>
    Co-authored-by: Abhinav Suman <abhi.suman@hotmail.com>

---
+++ app/src/main/java/com/hadiyarajesh/notex/ui/component/AllNotesComponents.kt
@@ -0,0 +1,88 @@
+package com.hadiyarajesh.notex.ui.component
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Divider
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.hadiyarajesh.notex.database.converter.InstantConverter
+import com.hadiyarajesh.notex.database.entity.Note
+import java.time.Instant
+
+@Composable
+fun NoteCard(note: Note) {
+    Card(
+        elevation = CardDefaults.cardElevation(),
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(start = 16.dp, end = 16.dp, top = 16.dp),
+        shape = RoundedCornerShape(16.dp)
+    ) {
+        Column(
+            verticalArrangement = Arrangement.Center,
+            modifier = Modifier.padding(16.dp)
+        ) {
+            note.title?.let {
+                TextSemiBold(
+                    content = it,
+                    null,
+                    MaterialTheme.typography.titleLarge, Color.Black
+                )
+            }
+            Row(
+                Modifier
+                    .fillMaxWidth()
+                    .padding(top = 16.dp)
+            ) {
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(IntrinsicSize.Min)
+                        .weight(1f)
+                ) {
+                    TextSemiBold(
+                        content = "Succeed", Modifier.padding(end = 8.dp)
+                    )
+                    Divider(
+                        color = Color.Gray, modifier = Modifier
+                            .fillMaxHeight()
+                            .width(1.dp)
+                    )
+                    TextSemiBold(content = "Goal", Modifier.padding(start = 8.dp))
+                }
+                Row(
+                    Modifier
+                        .fillMaxWidth()
+                        .weight(1f), horizontalArrangement = Arrangement.End
+                ) {
+                    TextSemiBold(
+                        content = InstantConverter.getLocalDate(note.createdOn).toString()
+                    )
+                }
+
+            }
+        }
+    }
+
+}
+
+@Preview
+@Composable
+fun NoteCardPrev() {
+    NoteCard(
+        Note(
+            noteId = 12345,
+            title = "Note title",
+            content = "Note content",
+            archived = false,
+            createdOn = Instant.now(),
+            updatedOn = Instant.now()
+        )
+    )
+}
