2423f28852ed8c9e7ba50263c73af385a1c23c3a
MosesATJTC <shaik@100ms.live>
2022-09-17 13:01:53 +0530

    Working on Notification and WorkManager

--- app/src/main/java/com/hadiyarajesh/notex/MainActivity.kt
+++ app/src/main/java/com/hadiyarajesh/notex/MainActivity.kt
@@ -2,11 +2,15 @@ package com.hadiyarajesh.notex
 
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
 import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import com.hadiyarajesh.notex.ui.NoteXApp
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -19,8 +23,29 @@ class MainActivity : ComponentActivity() {
         }
 
         createNotificationChannel()
+        createNotification()
     }
 
+    private fun createNotification(){
+
+        val fullScreenIntent = Intent(this, MainActivity::class.java)
+        val fullScreenPendingIntent = PendingIntent.getActivity(this, 0,
+            fullScreenIntent, PendingIntent.FLAG_UPDATE_CURRENT)
+
+        val builder = NotificationCompat.Builder(this, getString(R.string.notification_channel_id))
+            .setSmallIcon(R.drawable.ic_launcher_background)
+            .setContentTitle("My notification")
+            .setContentText("Hello World!")
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .setFullScreenIntent(fullScreenPendingIntent, true)
+            .addAction(R.drawable.ic_note_filled,"Snooze",fullScreenPendingIntent)
+            .addAction(R.drawable.ic_note_filled,"Snooze",fullScreenPendingIntent)
+
+        with(NotificationManagerCompat.from(this)) {
+            // notificationId is a unique int for each notification that you must define
+            notify(1234, builder.build())
+        }
+    }
     private fun createNotificationChannel() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             val channelId = getString(R.string.notification_channel_id)
--- app/src/main/java/com/hadiyarajesh/notex/MyApplication.kt
+++ app/src/main/java/com/hadiyarajesh/notex/MyApplication.kt
@@ -1,7 +1,21 @@
 package com.hadiyarajesh.notex
 
 import android.app.Application
+import androidx.hilt.work.HiltWorkerFactory
+import androidx.work.Configuration
 import dagger.hilt.android.HiltAndroidApp
+import javax.inject.Inject
 
 @HiltAndroidApp
-class MyApplication : Application()
+class MyApplication : Application(),Configuration.Provider{
+
+    @Inject
+    lateinit var workerFactory: HiltWorkerFactory
+
+    override fun getWorkManagerConfiguration(): Configuration {
+        return Configuration.Builder()
+            .setWorkerFactory(workerFactory)
+            .build()
+    }
+
+}
