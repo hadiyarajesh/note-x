9d1c6705d7a9749a69e6c602b330a77c8246c2c9
Arunshaik2001 <arunshaik432@gmail.com>
2022-09-22 17:40:36 +0530

    refactoring

--- app/src/main/java/com/hadiyarajesh/notex/reminder/notification/NotificationBroadCastReceiver.kt
+++ app/src/main/java/com/hadiyarajesh/notex/reminder/notification/NotificationBroadCastReceiver.kt
@@ -3,6 +3,7 @@ package com.hadiyarajesh.notex.reminder.notification
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.util.Log
 import androidx.annotation.CallSuper
 import androidx.core.app.NotificationManagerCompat
 import com.hadiyarajesh.notex.R
@@ -15,34 +16,44 @@ import javax.inject.Inject
 @AndroidEntryPoint
 class NotificationBroadCastReceiver : HiltBroadcastReceiver() {
 
+    companion object {
+        const val TAG = "NotificationBroadCast"
+    }
+
     @Inject
     lateinit var reminderWorkManager: ReminderWorkManager
 
 
     override fun onReceive(context: Context, intent: Intent) {
         super.onReceive(context, intent)
-        if (intent.action == context.resources.getString(R.string.done_action)) {
-            reminderWorkManager.cancelWorkRequest(
-                context,
-                intent.getStringExtra(context.resources.getString(R.string.worker_tag)) ?: ""
-            )
-        } else if (intent.action == context.resources.getString(R.string.postpone_action)) {
-
-            reminderWorkManager.cancelWorkRequest(
-                context,
-                intent.getStringExtra(context.resources.getString(R.string.worker_tag)) ?: ""
-            )
-
-            reminderWorkManager.createWorkRequestAndEnqueue(
-                context,
-                time = Instant.now().plus(1, ChronoUnit.HOURS),
-                isFirstTime = false,
-                reminderId = intent.getLongExtra(
-                    context.resources.getString(R.string.reminder_id),
-                    -1
+        when (intent.action) {
+            context.resources.getString(R.string.done_action) -> {
+                reminderWorkManager.cancelWorkRequest(
+                    context,
+                    intent.getStringExtra(context.resources.getString(R.string.worker_tag)) ?: ""
                 )
-            )
+            }
+            context.resources.getString(R.string.postpone_action) -> {
+                reminderWorkManager.cancelWorkRequest(
+                    context,
+                    intent.getStringExtra(context.resources.getString(R.string.worker_tag)) ?: ""
+                )
+
+                reminderWorkManager.createWorkRequestAndEnqueue(
+                    context,
+                    time = Instant.now().plus(1, ChronoUnit.HOURS),
+                    isFirstTime = false,
+                    reminderId = intent.getLongExtra(
+                        context.resources.getString(R.string.reminder_id),
+                        -1
+                    )
+                )
+            }
+            else -> {
+                Log.i(TAG, "Nothing to Perform this Action ${intent.action}")
+            }
         }
+
         with(NotificationManagerCompat.from(context)) {
             cancel(intent.getIntExtra(context.resources.getString(R.string.notification_id), -1))
         }
