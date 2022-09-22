d7a5166f0ae84a5f1abb4e6c2d99f5eb7dbe6c90
Arunshaik2001 <arunshaik432@gmail.com>
2022-09-22 21:48:51 +0530

    changed NoteXApp to myContent

--- app/src/main/java/com/hadiyarajesh/notex/MainActivity.kt
+++ app/src/main/java/com/hadiyarajesh/notex/MainActivity.kt
@@ -2,16 +2,12 @@ package com.hadiyarajesh.notex
 
 import android.app.NotificationChannel
 import android.app.NotificationManager
-import android.app.PendingIntent
 import android.content.Context
-import android.content.Intent
 import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.core.app.NotificationCompat
-import androidx.core.app.NotificationManagerCompat
-import com.hadiyarajesh.notex.ui.NoteXApp
+import com.hadiyarajesh.notex.ui.reminders.myContent
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -19,7 +15,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            NoteXApp()
+            myContent()
         }
 
         createNotificationChannel()
