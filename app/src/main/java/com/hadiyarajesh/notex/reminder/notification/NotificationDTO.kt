bfa220a9d1b9932dfebad6efc7f6c81fed3f83ff
Arunshaik2001 <arunshaik432@gmail.com>
2022-09-24 09:28:26 +0530

    added NotificationDTO.kt

---
+++ app/src/main/java/com/hadiyarajesh/notex/reminder/notification/NotificationDTO.kt
@@ -0,0 +1,9 @@
+package com.hadiyarajesh.notex.reminder.notification
+
+data class NotificationDTO(
+    val title: String,
+    val subTitle: String,
+    val reminderId: Long,
+    val workerTag: String
+) {
+}
